60fe1b5a3e1d67fb980a170618ab36ccf8420fb3
Mohamed Behhar <mbehhar@e3r8p17.1337.ma>
2024-04-24 21:45:10 +0100

    adding booking notification notification

---
+++ backend/src/controllers/bookCarpoolingControllers.js
@@ -0,0 +1,19 @@
+const bookCarpoolingService = require('../services/bookCarpoolingServices');
+
+const bookCarpooling = async (req, res) => {
+    const { user_id, carpooling_id, numberOfSeats } = req.body;
+    try {
+        const booking = await bookCarpoolingService.bookCarpooling({ user_id, carpooling_id, numberOfSeats });
+
+        io.emit('newBooking', booking);
+
+        res.status(200).json(booking);
+    } catch (error) {
+		console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+}
+
+module.exports = {
+	bookCarpooling,
+}
